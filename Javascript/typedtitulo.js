00dd9d8abc7b53cab861545d624897f42c292d02
RobertoLunaSalas <lunasalasr@gmail.com>
2022-11-14 22:16:34 -0600

    Titulo animado

---
+++ Javascript/typedtitulo.js
@@ -0,0 +1,17 @@
+const typed = new Typed('.typed',{
+    strings: [
+        '<i class="palani">Windows 11</i>',
+        '<i class="palani">el mejor SO</i>',
+    ],
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    shuffle: true,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
